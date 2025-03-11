21f1ec16f923682b52b48ea09bac8c67c632d302
lukasuxo <lukasukhiashvili06@gmail.com>
2025-03-12 00:37:24 +0400

    ..

--- src/components/molecules/CardDetails.jsx
+++ src/components/molecules/CardDetails.jsx
@@ -3,49 +3,50 @@ import InputField from "../atoms/InputField";
 const CardDetails = ({ formData, handleChange }) => {
   return (
     <div>
-      <InputField 
-        label="Cardholder Name" 
-        name="name" 
-        value={formData.name} 
-        onChange={handleChange} 
+      <InputField
+        label="Cardholder Name"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
         placeholder="e.g. Jane Appleseed"
+        maxLength={27}
       />
 
-      <InputField 
-        label="Card Number" 
-        name="cardNumber" 
-        value={formData.cardNumber} 
-        onChange={handleChange} 
-        placeholder="e.g. 1234 5678 9123 0000" 
-        validation={(val) => /^\d*$/.test(val)} 
+      <InputField
+        label="Card Number"
+        name="cardNumber"
+        value={formData.cardNumber}
+        onChange={handleChange}
+        placeholder="e.g. 1234 5678 9123 0000"
+        validation={(val) => /^\d*$/.test(val)}
         maxLength={16}
       />
 
       <div>
-        <InputField 
-          label="Exp. Month" 
-          name="expiryMonth" 
-          value={formData.expiryMonth} 
-          onChange={handleChange} 
-          placeholder="MM" 
+        <InputField
+          label="Exp. Month"
+          name="expiryMonth"
+          value={formData.expiryMonth}
+          onChange={handleChange}
+          placeholder="MM"
           maxLength={2}
         />
-        <InputField 
-          label="Exp. Year" 
-          name="expiryYear" 
-          value={formData.expiryYear} 
-          onChange={handleChange} 
-          placeholder="YY" 
+        <InputField
+          label="Exp. Year"
+          name="expiryYear"
+          value={formData.expiryYear}
+          onChange={handleChange}
+          placeholder="YY"
           maxLength={2}
         />
       </div>
-      <InputField 
-        label="CVC" 
-        name="cvc" 
-        value={formData.cvc} 
-        onChange={handleChange} 
-        placeholder="e.g. 123" 
-        validation={(val) => /^\d*$/.test(val)} 
+      <InputField
+        label="CVC"
+        name="cvc"
+        value={formData.cvc}
+        onChange={handleChange}
+        placeholder="e.g. 123"
+        validation={(val) => /^\d*$/.test(val)}
         maxLength={3}
       />
     </div>
