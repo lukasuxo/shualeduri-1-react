c669286ba93033ab9a6426477ad5c24da524c056
lukasuxo <lukasukhiashvili06@gmail.com>
2025-03-12 00:24:14 +0400

    .

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import CheckoutPage from "./components/template/CheckoutPage";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div>
+      <CheckoutPage />
+    </div>
+  );
+}
+
+export default App;
+
+
+
